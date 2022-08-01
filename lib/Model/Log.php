467af1a7190712c6caa1eed86571ed9367d62070
c-okada <hello.tra.okada@gmail.com>
2022-08-01 10:57:10 +0900

    テーブル定義変更成功

--- lib/Model/Log.php
+++ lib/Model/Log.php
@@ -4,18 +4,18 @@ class Log extends \Bbs\Model {
   public function createLog($values){
     try{
       $this->db->beginTransaction();
-      $sql = "INSERT INTO log (user_id,action,created,modified) VALUES (:user_id,:action,now(),now())";
+      $sql = "INSERT INTO timer (user_id,start,finish) VALUES (:user_id,:start,:finish)";
       $stmt = $this->db->prepare($sql);
-      $stmt->bindValue('user_id',$values['user_id']);
-      $stmt->bindValue('action',$values['action']);
-      $res = $stmt->execute();
-      $log_id = $this->db->lastInsertId();
-      $sql = "INSERT INTO timer (user_id,log_id,start,finish,time) VALUES (:user_id,:log_id,:start,:finish,:time)";
-      $stmt = $this->db->prepare($sql);
-      $stmt->bindValue('log_id',$log_id);
       $stmt->bindValue('user_id',$values['user_id']);
       $stmt->bindValue('start',$values['start']);
       $stmt->bindValue('finish',$values['finish']);
+      $res = $stmt->execute();
+      $timer_id = $this->db->lastInsertId();
+      $sql = "INSERT INTO log (user_id,timer_id,action,time,created,modified) VALUES (:user_id,:timer_id,:action,:time,now(),now())";
+      $stmt = $this->db->prepare($sql);
+      $stmt->bindValue('user_id',$values['user_id']);
+      $stmt->bindValue('timer_id',$timer_id);
+      $stmt->bindValue('action',$values['action']);
       $stmt->bindValue('time',$values['time']);
       $res = $stmt->execute();
       $this->db->commit();
